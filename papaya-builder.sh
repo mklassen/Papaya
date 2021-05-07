8fe2d0da6c4551db024eb6354258f3d02f2af4b4
Martyn Klassen <lmklassen@gmail.com>
2021-05-07 11:26:23 -0400

    Updated the compiler to latest google closure compiler

--- papaya-builder.sh
+++ papaya-builder.sh
@@ -19,7 +19,7 @@ java -Xmx512M -jar lib/papaya-builder.jar "$@"
 if [ -f build/papaya.js ]
 then
     echo Further compressing...
-    java -jar lib/compiler.jar --warning_level QUIET --compilation_level SIMPLE_OPTIMIZATIONS --language_in=ECMASCRIPT5 --js build/papaya.js --js_output_file build/papaya-out.js
+    java -jar lib/compiler.jar --warning_level QUIET --compilation_level SIMPLE_OPTIMIZATIONS --language_in=ECMASCRIPT_2020 --js build/papaya.js --js_output_file build/papaya-out.js
     rm build/papaya.js
     mv build/papaya-out.js build/papaya.js
 fi
